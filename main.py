8e1702a8f77f82a91f11f946ff7c8fc7129c2816
Pascal Nosike <108730819+gbreigns@users.noreply.github.com>
2023-06-27 09:58:31 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+from scissor import app
+
+if __name__ == '__main__':
+    app.run()
---
+++ scissor/__init__.py
@@ -0,0 +1,55 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_caching import Cache
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from dotenv import load_dotenv
+import os, qrcode
+
+
+app = Flask(__name__)
+
+load_dotenv()
+
+base_dir = os.path.dirname(os.path.realpath(__file__))
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+#     os.path.join(base_dir, 'scissor.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['CACHE_TYPE'] = 'SimpleCache'
+app.config['CACHE_DEFAULT_TIMEOUT'] = 300
+app.config.update(
+    UPLOAD_PATH = os.path.join(base_dir, 'static', 'qr-codes')
+)
+
+
+cache = Cache(app)
+limiter = Limiter(get_remote_address)
+
+qr = qrcode.QRCode(
+    version = 1,
+    error_correction = qrcode.constants.ERROR_CORRECT_L,
+    box_size = 5,
+    border = 4
+)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+with app.app_context():
+    db.create_all()
+
+from . import routes
+from .models import User
